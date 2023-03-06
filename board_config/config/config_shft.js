ba5a2294dd7f8fa1a37f42dcbb16a0e1df7353e7
Seymour Roberts <seymour2@pdx.edu>
2023-03-05 17:02:28 -0800

    edit/refactor: refactor en_sqr_hght
    minor refactor includes: undo_en_sqr_hght/hght_sqrs/convertPointToSquare

--- board_config/config/config_shft.js
+++ board_config/config/config_shft.js
@@ -2,23 +2,35 @@ import { rtrn_shift_grid } from "./grid/generate.js";
 
 var prv_sqr;
 var sqrsTo_hght;
-var prv_sqr_point;
+var crnt_sqr_point;
 const sqr_shft = {};
 const shft_audio = document.getElementById("shft");
 const erase_audio = document.getElementById("erase");
-const hght =
-  "rgba(13, 114, 228, 0.7) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset";
+const hght = "rgba(13, 114, 228, 0.7) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset";
 
 export function en_sqr_hght() {
-  if (chdrn_sqr_chk({ id: this.id }) !== true) return;
-  prv_sqr_point = this.id;
-  prv_sqr = document.getElementById(this.id);
+  if (chdrn_sqr_chk({ id: this.id })) {
+  	crnt_sqr_point = this.id;
+  	prv_sqr = convertPointToSquare({point:this.id});
+		undo_en_sqr_hght();
+ 		hght_sqrs(); 
+	}
+}
 
-  prv_sqr.removeEventListener("click", en_sqr_hght);
-  //sqrsTo_hght = wht_knght_grid[this.id];
+function convertPointToSquare({point}) {
+	return document.getElementById(point);
+}
+
+function undo_en_sqr_hght() {
+	prv_sqr.removeEventListener("click", en_sqr_hght);
+}
+
+// remove the previous event handler(s) on each hght 
+function hght_sqrs() {
   const grid_search = prv_sqr.children[0].alt.replace(".svg", "").trim();
-  sqrsTo_hght = rtrn_shift_grid({ piece: grid_search, point: this.id });
-  sqrsTo_hght.forEach((point) => {
+  sqrsTo_hght = rtrn_shift_grid({piece: grid_search, point: crnt_sqr_point});
+
+	sqrsTo_hght.forEach((point) => {
     const sqr = document.getElementById(point);
     sqr.style.boxShadow = hght;
     sqr.removeEventListener("click", en_sqr_hght);
@@ -55,11 +67,11 @@ function en_chk() {
 
 // erase pn in prv sqr into nxt_sqr
 function erase({ nxt_shft }) {
-  const nxt_sqr = document.getElementById(nxt_shft);
+  const nxt = convertPointToSquare({point:nxt_shft}); 
 
   if (chdrn_sqr_chk({ id: nxt_shft }) === true) {
-    re_shft_erase({ nxt_sqr: nxt_sqr });
-  } else re_shft({ nxt_sqr: nxt_sqr });
+    re_shft_erase({nxt_sqr: nxt});
+  } else re_shft({nxt_sqr: nxt});
 }
 
 // rm pn in nxt_sqr once shfted
@@ -81,7 +93,7 @@ function re_shft({ nxt_sqr }) {
 // discard highligting on prv_sqr
 function un_en_chk() {
   sqrsTo_hght.forEach((point) => {
-    const sqr = document.getElementById(point);
+    const sqr = convertPointToSquare({point:point});
     sqr.removeEventListener("click", en_chk);
   });
 }
