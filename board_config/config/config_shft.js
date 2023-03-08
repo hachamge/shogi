b55af353d22d3a34f3911103c6cb9704bf394ceb
Seymour Roberts <seymour2@pdx.edu>
2023-03-07 17:44:25 -0800

    edit/refactor: refactor hght_sqrs to pass off the parsing to the rtrn_shft_grid function
    in addition discard unused variables: prv_sqr_point, and rename prv_sqr to crnt_sqr

--- board_config/config/config_shft.js
+++ board_config/config/config_shft.js
@@ -1,17 +1,14 @@
 import { rtrn_shift_grid } from "./grid/generate.js";
 
-var prv_sqr;
+var crnt_sqr;
 var sqrsTo_hght;
-var crnt_sqr_point;
-const sqr_shft = {};
 const shft_audio = document.getElementById("shft");
 const erase_audio = document.getElementById("erase");
 const hght = "rgba(13, 114, 228, 0.7) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset";
 
 export function en_sqr_hght() {
   if (chdrn_sqr_chk({ id: this.id })) {
-  	crnt_sqr_point = this.id;
-  	prv_sqr = convertPointToSquare({point:this.id});
+  	crnt_sqr = convertPointToSquare({point:this.id});
 		undo_en_sqr_hght();
  		hght_sqrs(); 
 	}
@@ -22,13 +19,12 @@ function convertPointToSquare({point}) {
 }
 
 function undo_en_sqr_hght() {
-	prv_sqr.removeEventListener("click", en_sqr_hght);
+	crnt_sqr.removeEventListener("click", en_sqr_hght);
 }
 
 // remove the previous event handler(s) on each hght 
 function hght_sqrs() {
-  const grid_search = prv_sqr.children[0].alt.replace(".svg", "").trim();
-  sqrsTo_hght = rtrn_shift_grid({piece: grid_search, point: crnt_sqr_point});
+  sqrsTo_hght = rtrn_shift_grid({sqr: crnt_sqr});
 
 	sqrsTo_hght.forEach((point) => {
     const sqr = document.getElementById(point);
@@ -77,20 +73,20 @@ function erase({ nxt_shft }) {
 // rm pn in nxt_sqr once shfted
 function re_shft_erase({ nxt_sqr }) {
   erase_audio.play();
-  nxt_sqr.children[0].src = prv_sqr.children[0].src;
-  nxt_sqr.children[0].alt = prv_sqr.children[0].alt;
-  prv_sqr.children[0].remove();
+  nxt_sqr.children[0].src = crnt_sqr.children[0].src;
+  nxt_sqr.children[0].alt = crnt_sqr.children[0].alt;
+  crnt_sqr.children[0].remove();
   un_en_chk();
 }
 
 // shft pn in prv sqr into the nxt_sqr
 function re_shft({ nxt_sqr }) {
   shft_audio.play();
-  nxt_sqr.append(prv_sqr.children[0]);
+  nxt_sqr.append(crnt_sqr.children[0]);
   un_en_chk();
 }
 
-// discard highligting on prv_sqr
+// discard highligting on crnt_sqr
 function un_en_chk() {
   sqrsTo_hght.forEach((point) => {
     const sqr = convertPointToSquare({point:point});
--- board_config/config/grid/generate.js
+++ board_config/config/grid/generate.js
@@ -2,16 +2,16 @@ import { wht_rk_grid } from "./wht_rk_grid.js";
 import { knght_grid } from "./wht_knght_grid.js";
 
 export const shift_grid = {};
-shift_grid["wht_rk"] = wht_rk_grid;
+shift_grid["rk"] = wht_rk_grid;
 shift_grid["knght"] = knght_grid;
 
-function prs_piece({piece}) {
-	 return piece.substring(4, piece.length);
+function prs_piece({ sqr }) {
+  var img_str = sqr.children[0].alt.replace(".svg", "").trim();
+  return img_str.substring(4, img_str.length);
 }
 
-export function rtrn_shift_grid({ piece, point }) {
-	piece = prs_piece({piece:piece});
-  const grid = shift_grid[piece];
-  return grid[point];
+export function rtrn_shift_grid({ sqr }) {
+  var prs_img_alt = prs_piece({ sqr: sqr });
+  const grid = shift_grid[prs_img_alt];
+  return grid[sqr.id];
 }
-
