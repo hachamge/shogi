7cfd2a99943f98bc26b2a788976eb441c4eba91f
Seymour Roberts <seymour2@pdx.edu>
2023-03-09 15:23:26 -0800

    edit/config: wrote initial code to create graph #1
    the class coordinate_graph is able to render a div with datalist/input

---
+++ board_config/charts/coordinate_graph.js
@@ -0,0 +1,42 @@
+//'use strict'
+
+export class coordinate_graph {
+	constructor (id) {
+		this.id = id;
+		this.graphs = new Array();
+		this.pieces = ['knght', 'kng', 'rook', 'bishop', 'queen', 'pawn'];
+	} 
+
+	// datalist of pieces kngnt/kng .. 
+	selection () {
+		const dl = document.createElement('datalist');
+		this.pieces.forEach(piece => {
+				const op = document.createElement('option');
+				op.setAttribute('value', piece);
+				dl.append(op); 
+		});
+		dl.setAttribute('id', 'coordinate_graph#1');
+		return dl;
+	} 
+
+	// create container: graph, datalist 
+	 async create_container () {
+		const time = document.createElement('p');
+		const div = document.createElement('div');
+		const graph = document.createElement('div');
+	 	const input = document.createElement('input');
+
+		graph.setAttribute('id', this.id);
+		time.innerHTML = 'updated 2 min ago';
+		div.setAttribute('class', 'coordinate_graph');
+		input.setAttribute('list', 'coordinate_graph#1');
+
+		const dl = await this.selection(); 
+		div.append(time);
+		div.append(dl);
+		div.append(graph);
+		div.prepend(input);
+		this.graphs.push(div);
+		document.querySelector('.shogi').append(div);
+	}
+}
