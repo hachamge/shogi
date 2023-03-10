701bb53840f275ed904fd5b8e20ed05ac8556aa4
Seymour Roberts <seymour2@pdx.edu>
2023-03-09 23:26:19 -0800

    edit/prettier: format coordinate_graph.js

--- board_config/charts/coordinate_graph.js
+++ board_config/charts/coordinate_graph.js
@@ -1,45 +1,45 @@
-import { graph } from './graph.js';
+import { graph } from "./graph.js";
 
 export class coordinate_graph {
-	constructor (id) {
-		this.id = id;
-		this.graph = graph;
-		this.graphs = new Array();
-		this.pieces = ['knght', 'kng', 'rook', 'bishop', 'queen', 'pawn'];
-	} 
+  constructor(id) {
+    this.id = id;
+    this.graph = graph;
+    this.graphs = new Array();
+    this.pieces = ["knght", "kng", "rook", "bishop", "queen", "pawn"];
+  }
 
-	// datalist of pieces kngnt/kng .. 
-	selection () {
-		const dl = document.createElement('datalist');
-		this.pieces.forEach(piece => {
-				const op = document.createElement('option');
-				op.setAttribute('value', piece);
-				dl.append(op); 
-		});
-		dl.setAttribute('id', 'coordinate_graph#1');
-		return dl;
-	} 
+  // datalist of pieces kngnt/kng ..
+  selection() {
+    const dl = document.createElement("datalist");
+    this.pieces.forEach((piece) => {
+      const op = document.createElement("option");
+      op.setAttribute("value", piece);
+      dl.append(op);
+    });
+    dl.setAttribute("id", "coordinate_graph#1");
+    return dl;
+  }
 
-	// create container: graph, datalist 
-	 async create_container () {
-		const time = document.createElement('p');
-		const div = document.createElement('div');
-		const graph = document.createElement('div');
-	 	const input = document.createElement('input');
+  // create container: graph, datalist
+  async create_container() {
+    const time = document.createElement("p");
+    const div = document.createElement("div");
+    const graph = document.createElement("div");
+    const input = document.createElement("input");
 
-		graph.setAttribute('id', this.id);
-		input.setAttribute('value', 'knight');
-		time.innerHTML = 'updated 2 min ago';
-		div.setAttribute('class', 'coordinate_graph');
-		input.setAttribute('list', 'coordinate_graph#1');
+    graph.setAttribute("id", this.id);
+    input.setAttribute("value", "knight");
+    time.innerHTML = "updated 2 min ago";
+    div.setAttribute("class", "coordinate_graph");
+    input.setAttribute("list", "coordinate_graph#1");
 
-		const dl = await this.selection(); 
-		div.append(time);
-		div.append(dl);
-		div.append(graph);
-		div.prepend(input);
-		this.graphs.push(div);
-		document.querySelector('.shogi').append(div);
-		this.graph({id:`#${this.id}`});
-	}	
+    const dl = await this.selection();
+    div.append(time);
+    div.append(dl);
+    div.append(graph);
+    div.prepend(input);
+    this.graphs.push(div);
+    document.querySelector(".shogi").append(div);
+    this.graph({ id: `#${this.id}` });
+  }
 }
