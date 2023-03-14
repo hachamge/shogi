ab640dab7dee8f96b0f15d916b54da383426a13c
Seymour Roberts <seymour2@pdx.edu>
2023-03-14 16:31:39 -0700

    edit/config: redesign the former (contacts) now 'host', class
    2 instances are left for further testing until done

--- scripts/contact/contacts.js
+++ scripts/contact/contacts.js
@@ -1,35 +1,69 @@
-function config({timeAgo, id}) {
-	const img = document.createElement('h6');
-	const strg = document.createElement('div');
-	const time = document.createElement('h4');
-	const msg = document.createElement('h4');
-	const shift = document.createElement('p');
-	const nme = document.createElement('h3');
-	const n_strg = document.createElement('div');
-	
-	strg.setAttribute('class', 'contact');
-	time.innerText = timeAgo;
-	shift.innerText = 'knight e5';
-	msg.innerText = 'perfect knight shift. chihyi return!';
-	nme.innerText = 'Harrison Orne';
-	n_strg.setAttribute('class', 'n_strg');
-	img.setAttribute('id', 'co');
+class host {
+  // id referes to div_id
+  constructor(img, timeAgo, nme, msg, shift, id) {
+    this.id = id;
+    this.img = img;
+    this.nme = nme;
+    this.msg = msg;
+    this.shift = shift;
+    this.timeAgo = timeAgo;
+		this.tagsTo_create = {img:'img', timeAgo: 'h3', nme:'h2', msg: 'h3', shft: 'p', div:'div'};
+  }
 
-	nme.prepend(img);
-	n_strg.append(img);
-	n_strg.append(nme);
-	
-	strg.append(time);
-	strg.append(n_strg);
-	strg.append(msg);
-	strg.append(shift);
+  config_tagsTo_create() {
+    for (const tag in this.tagsTo_create) {
+      const html_tg = document.createElement(this.tagsTo_create[tag]);
+      this.tagsTo_create[tag] = html_tg;
+    }
+    this.init_tags_created();
+    this.appendTo_div();
+    this.append({ id: this.id });
+  }
 
-	document.getElementById(id).append(strg);
-}
+  init_tags_created() {
+    this.tagsTo_create["img"].src = this.img;
+    this.tagsTo_create["nme"].innerText = this.nme;
+    this.tagsTo_create["msg"].innerText = this.msg;
+    this.tagsTo_create["shft"].innerText = this.shift;
+    this.tagsTo_create["timeAgo"].innerText = this.timeAgo;
+  }
 
-config({timeAgo:'2 min ago', id: 'cont'});
-config({timeAgo:'2 min ago', id: 'cont'});
-config({timeAgo:'2 min ago', id: 'cont'});
-config({timeAgo:'2 min ago', id: 'cont'});
-config({timeAgo:'2 min ago', id: 'cont'});
-config({timeAgo:'2 min ago', id: 'cont'});
+  appendTo_div() {
+    for (const tag in this.tagsTo_create) {
+      if (this.tagsTo_create[tag] instanceof HTMLImageElement) continue;
+      else if (this.tagsTo_create[tag] instanceof HTMLDivElement) continue;
+      else this.tagsTo_create["div"].append(this.tagsTo_create[tag]);
+    }
+    this.tagsTo_create["div"].setAttribute("id", "host_info");
+  }
+
+  append({ id }) {
+    const host = document.createElement("div");
+    host.append(this.tagsTo_create["img"]);
+    host.append(this.tagsTo_create["div"]);
+    host.setAttribute("class", "host");
+    console.log(host);
+    document.getElementById(id).append(host);
+  }
+} // #endif
+
+const cll = new host(
+  "../../svg/beach.jpg",
+  "2 min ago",
+  "Harrison Orne",
+  "perfect knight move",
+  "knight e5",
+  "ht"
+);
+
+const cll2 = new host(
+  "../../svg/flower.jpg",
+  "5 min ago",
+  "Steffen Holm",
+  "shift into h3",
+  "rook h5",
+  "ht"
+);
+//cll.init_tagsTo_create();
+cll.config_tagsTo_create();
+cll2.config_tagsTo_create();
