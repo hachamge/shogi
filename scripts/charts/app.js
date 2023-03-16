1e49cdb2db325d812101673ca6f9d893b852d159
Seymour Roberts <seymour2@pdx.edu>
2023-03-16 15:44:59 -0700

    minor/edit: instantiate a coordinate_graph object

--- scripts/charts/app.js
+++ scripts/charts/app.js
@@ -1,3 +1,4 @@
 import { coordinate_graph } from './coordinate_graph.js';
 
-//const config = new coordinate_graph('#graphLayout');
+const config = new coordinate_graph('#graph1');
+config.create_container();
