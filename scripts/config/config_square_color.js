76f818ae1073da7b16d9ae97a6fb530ad767c73b
Seymour Roberts <seymour2@pdx.edu>
2023-03-19 13:05:52 -0700

    Revert "edit/undo: undo delete to config_square color"
    This reverts commit 063ebc564678f99be294bcf1c7bc25951bbd1fe9.

---
+++ scripts/config/config_square_color.js
@@ -0,0 +1,74 @@
+export const sqr_color = {}; 
+sqr_color['a8'] = '#f5f5f5';//wht
+sqr_color['b8'] = '#28282B';
+sqr_color['c8'] = '#f5f5f5';
+sqr_color['d8'] = '#28282B';
+sqr_color['e8'] = '#f5f5f5';
+sqr_color['f8'] = '#28282B';
+sqr_color['g8'] = '#f5f5f5';
+sqr_color['h8'] = '#28282B';
+
+sqr_color['a7'] = '#28282B';//drk
+sqr_color['b7'] = '#f5f5f5';
+sqr_color['c7'] = '#28282B';
+sqr_color['d7'] = '#f5f5f5';
+sqr_color['e7'] = '#28282B';
+sqr_color['f7'] = '#f5f5f5';
+sqr_color['g7'] = '#28282B';
+sqr_color['h7'] = '#f5f5f5';
+
+sqr_color['a6'] = '#f5f5f5';//wht
+sqr_color['b6'] = '#28282B';
+sqr_color['c6'] = '#f5f5f5';
+sqr_color['d6'] = '#28282B';
+sqr_color['e6'] = '#f5f5f5';
+sqr_color['f6'] = '#28282B';
+sqr_color['g6'] = '#f5f5f5';
+sqr_color['h6'] = '#28282B';
+
+sqr_color['a5'] = '#28282B';//drk
+sqr_color['b5'] = '#f5f5f5';
+sqr_color['c5'] = '#28282B';
+sqr_color['d5'] = '#f5f5f5';
+sqr_color['e5'] = '#28282B';
+sqr_color['f5'] = '#f5f5f5';
+sqr_color['g5'] = '#28282B';
+sqr_color['h5'] = '#f5f5f5';
+
+sqr_color['a4'] = '#f5f5f5';//wht
+sqr_color['b4'] = '#28282B';
+sqr_color['c4'] = '#f5f5f5';
+sqr_color['d4'] = '#28282B';
+sqr_color['e4'] = '#f5f5f5';
+sqr_color['f4'] = '#28282B';
+sqr_color['g4'] = '#f5f5f5';
+sqr_color['h4'] = '#28282B';
+
+sqr_color['a3'] = '#28282B';//drk
+sqr_color['b3'] = '#f5f5f5';
+sqr_color['c3'] = '#28282B';
+sqr_color['d3'] = '#f5f5f5';
+sqr_color['e3'] = '#28282B';
+sqr_color['f3'] = '#f5f5f5';
+sqr_color['g3'] = '#28282B';
+sqr_color['h3'] = '#f5f5f5';
+
+sqr_color['a2'] = '#f5f5f5';//wht
+sqr_color['b2'] = '#28282B';
+sqr_color['c2'] = '#f5f5f5';
+sqr_color['d2'] = '#28282B';
+sqr_color['e2'] = '#f5f5f5';
+sqr_color['f2'] = '#28282B';
+sqr_color['g2'] = '#f5f5f5';
+sqr_color['h2'] = '#28282B';
+
+sqr_color['a1'] = '#28282B';//drk
+sqr_color['b1'] = '#f5f5f5';
+sqr_color['c1'] = '#28282B';
+sqr_color['d1'] = '#f5f5f5';
+sqr_color['e1'] = '#28282B';
+sqr_color['f1'] = '#f5f5f5';
+sqr_color['g1'] = '#28282B';
+sqr_color['h1'] = '#f5f5f5';
+
+// enable for jest: module.exports = sqr_color;
